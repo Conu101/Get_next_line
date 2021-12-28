31b6e0dc685382a9389926ce0cc6c7193de8036c
Conu101 <90414327+Conu101@users.noreply.github.com>
2021-12-28 17:56:58 +0200

    Add files via upload

---
+++ get_next_line.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ctrouve <ctrouve@student.hive.fi>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/28 17:03:49 by ctrouve           #+#    #+#             */
+/*   Updated: 2021/12/28 17:07:53 by ctrouve          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# define BUFF_SIZE 8
+# define FD_SIZE 8192
+
+# include "libft/libft.h"
+
+int	get_next_line(const int fd, char **line);
+
+#endif
